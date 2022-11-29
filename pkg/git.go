759ab46f36813907daea9aedd11c250c9fe991fc
Drew Welch <dwelch@redhat.com>
2022-11-29 16:06:42 -0600

    Refactor approach for importing internal git server cert (#18)
    * refactor method for importing internal git server cert
    
    * move env var check outside loop
    
    * update mounts

--- pkg/git.go
+++ pkg/git.go
@@ -3,25 +3,43 @@ package pkg
 import (
 	"fmt"
 	"net/url"
+	"os"
 	"os/exec"
 )
 
 // Push local repos to remotes
 func (d *Downloader) pushLatest(archives []*UntarInfo) error {
-	for _, archive := range archives {
+	// include command to trust internal git server certificate if set
+	caPath := os.Getenv("INTERNAL_GIT_CA_PATH")
 
+	for _, archive := range archives {
 		authURL, err := d.formatAuthURL(fmt.Sprintf("%s/%s", archive.RemoteGroup, archive.RemoteName))
 		if err != nil {
 			return err
 		}
 
-		args := []string{
-			"-c",
-			fmt.Sprintf("%s && %s",
-				fmt.Sprintf("git remote add fedramp %s", authURL),
-				fmt.Sprintf("git push -u fedramp %s --force", archive.RemoteBranch),
-			),
+		var args []string
+		if len(caPath) > 0 {
+			args = []string{
+				"-c",
+				fmt.Sprintf("%s && %s && %s",
+					// this config must be set per repo (cannot be done once with --global flag)
+					// due to permission constraint when attempting to edit root gitconfig
+					fmt.Sprintf("git config http.sslCAInfo %s", caPath),
+					fmt.Sprintf("git remote add fedramp %s", authURL),
+					fmt.Sprintf("git push -u fedramp %s --force", archive.RemoteBranch),
+				),
+			}
+		} else {
+			args = []string{
+				"-c",
+				fmt.Sprintf("%s && %s",
+					fmt.Sprintf("git remote add fedramp %s", authURL),
+					fmt.Sprintf("git push -u fedramp %s --force", archive.RemoteBranch),
+				),
+			}
 		}
+
 		cmd := exec.Command("/bin/sh", args...)
 		cmd.Dir = archive.DirPath
 		err = cmd.Run()
